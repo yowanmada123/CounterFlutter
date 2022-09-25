ea012362dfc38185e1d83363206f932f84234d2a
yowanmada123 <byayoan@gmail.com>
2022-09-25 21:10:25 +0700

    third commit

--- lib/controllers/set_state_page.dart
+++ lib/controllers/set_state_page.dart
@@ -1,11 +1,6 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:get/get.dart';
-
-import 'main_controller.dart';
 
 class SetStatePage extends StatefulWidget {
   const SetStatePage({super.key});
